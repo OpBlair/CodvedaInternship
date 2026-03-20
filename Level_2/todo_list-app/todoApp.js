91b25440f8cc8c38a08c456c2e63c94cd238a58b
opblair <jusanotherthursday@gmail.com>
2026-03-20 10:53:07 +0300

    Added style for the task lists, functionality to form buttons

--- Level_2/todo_list-app/todoApp.js
+++ Level_2/todo_list-app/todoApp.js
@@ -5,6 +5,7 @@ const todoForm = document.getElementById('todo-form');
 const taskList = document.getElementById('task-list');
 const taskInput = document.getElementById('task-input');
 const cancelBtn = document.getElementById('cancel-btn');
+const date = document.getElementById('task-date');
 
 // Toggle Form visibilty
 showFormBtn.addEventListener('click', () => {
@@ -20,12 +21,31 @@ todoForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const taskName = taskInput.value;
+    const taskPriority = document.getElementById('task-priority').value;
+    const taskDate = date.value;
+    const formattedDate = taskDate ? taskDate.replace('T', ' ') : 'No deadline';
 
     const li = document.createElement('li');
-    li.textContent = taskName;
+    li.classList.add('taskName', `${taskPriority}-taskPriority`);
+
+    li.innerHTML = `
+        <div class="task-left">
+            <input type="checkbox" class="complete-checkbox">
+            <div class="task-details">
+                <span class="task-name">${taskName}</span>
+                <small class="task-time">${formattedDate}</small>
+                <small>Priority: ${taskPriority}</small>
+            </div>
+        </div>
+        <div class="task-right">
+            <button class="delete-btn">Delete</button>
+        </div>
+    `;
+
     taskList.appendChild(li);
 
     taskInput.value = '';
+    date.value = '';
     formOverlay.style.display = 'none';
 })
 
