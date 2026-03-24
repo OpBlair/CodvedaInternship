1e0116ba844a7ad23d2d2ca686af218c66b969c0
opblair <jusanotherthursday@gmail.com>
2026-03-25 00:38:53 +0300

    Added Function to createTask on the database.

--- Level_3/task-manager/backend/models/taskModel.js
+++ Level_3/task-manager/backend/models/taskModel.js
@@ -5,4 +5,19 @@ const getTasks = async () => {
     return result.rows;
 };
 
-module.export = { getTasks };
+const createTask = async (taskData) => {
+    try {
+        const sql = 'INSERT INTO tasks(taskName, description , category, priority, dueDate) VALUES ($1, $2, $3, $4, $5) RETURNING *';
+        
+        const values = [ taskData.name, taskData.description, taskData.category, taskData.priority, taskData.dueDate];
+
+        const result = await pool.query(sql, values);
+
+        return result.rows[0]; // return the row that was created.
+    } catch (err) {
+        console.error("Error creating task:", err.message);
+        throw err;
+    }
+}
+
+module.export = { getTasks, createTask }; // Export function so that other functions can use
