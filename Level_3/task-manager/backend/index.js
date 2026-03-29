932a155c3a849e7ca80f0bdf46d32855e785fd7b
opblair <jusanotherthursday@gmail.com>
2026-03-29 22:17:25 +0300

    update

--- Level_3/task-manager/backend/index.js
+++ Level_3/task-manager/backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.port || 3000; 
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173'
+    origin: [process.env.FRONTEND_URL, 'http://localhost:5173']
 }));
 app.use(express.json());
 
